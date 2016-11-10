fb9327cce84b8b880cd37337ef5e137a6a82fb41
Wes Gill <jahrik@gmail.com>
2016-11-09 20:34:01 -0700

    update

---
+++ roman-numerals.py
@@ -0,0 +1,29 @@
+def checkio(data):
+
+    ROMANS = (('M',  1000),
+              ('CM', 900),
+              ('D',  500),
+              ('CD', 400),
+              ('C',  100),
+              ('XC', 90),
+              ('L',  50),
+              ('XL', 40),
+              ('X',  10),
+              ('IX', 9),
+              ('V',  5),
+              ('IV', 4),
+              ('I',  1))
+
+    result = ""
+    for numeral, integer in ROMANS:
+        while data >= integer:
+            result += numeral
+            data -= integer
+    return result
+
+if __name__ == '__main__':
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert checkio(6) == 'VI', '6'
+    assert checkio(76) == 'LXXVI', '76'
+    assert checkio(499) == 'CDXCIX', '499'
+    assert checkio(3888) == 'MMMDCCCLXXXVIII', '3888'
